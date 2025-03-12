a604b392a43d37b6c1f56466f2ef15d66018e43c
veronikavanivska <veronikavanivska2005@gmail.com>
2025-03-12 19:14:07 +0100

    Add flow of hearts in lets'go page

--- index.js
+++ index.js
@@ -3,6 +3,40 @@ const question = qs(".question");
 const gif = qs(".gif");
 const [yesBtn, noBtn] = [".yes-btn", ".no-btn"].map(qs);
 
+
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.width = `${Math.floor(Math.random() * 65) + 10}px`;
+    heart.style.height = heart.style.width;
+    heart.style.left = `${Math.floor(Math.random() * 100) + 1}%`;
+    heart.style.background = `rgba(255, ${Math.floor(Math.random() * 25) + 100 - 25}, ${Math.floor(Math.random() * 25) + 100}, 1)`;
+    const duration = Math.floor(Math.random() * 5) + 5;
+    heart.style.animation = `love ${duration}s ease`;
+    return heart;
+}
+
+const container = document.querySelector('.bg_heart');
+
+function removeHearts() {
+    const hearts = container.querySelectorAll('.heart');
+    hearts.forEach((heart) => {
+        const top = parseFloat(getComputedStyle(heart).getPropertyValue('top'));
+        const width = parseFloat(getComputedStyle(heart).getPropertyValue('width'));
+        if (top <= -100 || width >= 150) {
+            heart.remove();
+        }
+    });
+}
+
+function addHeart() {
+    const heart1 = createHeart();
+    const heart2 = createHeart();
+    container.appendChild(heart1);
+    container.appendChild(heart2);
+    setTimeout(removeHearts, 1000);
+}
+
 const handleYesClick = () => {
     question.innerHTML = "Love you!!!!!!!!!!!!!";
     gif.src = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGRsbnQxdWVtNTMxejl1c2VjMnR5N203dmUxeG95OGdjOXowbzFxciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kVUyaJy5RZ6OCtRppP/giphy.gif";
@@ -39,7 +73,10 @@ const handleYesClick = () => {
         alert( selectedDateIdea);
     });
 
+    const love = setInterval(addHeart, 500);
+
     yesBtn.replaceWith(letsGoBtn);
+
 };
 
 const handleNoMouseOver = () => {
